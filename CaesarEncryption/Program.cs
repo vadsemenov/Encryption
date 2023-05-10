2ef9f373b1a6d6003919728bb5fef3415a978852
Vadim Semenov <semenov@comfortlab.ru>
2023-05-10 23:57:55 +0300

    Add project files.

---
+++ CaesarEncryption/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace CaesarEncryption
+{
+    public class Program
+    {
+        private static readonly char[] Alphabet = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку для шифрования:");
+
+            var sourceString = string.Empty;
+
+            while (sourceString == string.Empty)
+            {
+                sourceString = Console.ReadLine();
+            }
+
+            Console.WriteLine("Введите смещение алфавита:");
+
+            if (!int.TryParse(Console.ReadLine(), out var slideNumber))
+            {
+                return;
+            }
+
+            var encryptedString = CryptString(sourceString!, slideNumber);
+            Console.WriteLine("Строка после шифрования:");
+            Console.WriteLine(encryptedString);
+
+            var decryptedString = DecryptString(encryptedString, slideNumber);
+            Console.WriteLine("Расшифрованная строка:");
+            Console.WriteLine(decryptedString);
+        }
+
+        private static string CryptString(string sourceString, int slideNumber)
+        {
+            var encryptedString = string.Empty;
+
+            foreach (var ch in sourceString)
+            {
+                if (Alphabet.Contains(ch))
+                {
+                    var positionNumber = GetCharPosition(ch);
+                    positionNumber += slideNumber % Alphabet.Length;
+
+                    if (positionNumber >= Alphabet.Length)
+                    {
+                        positionNumber -= Alphabet.Length;
+                    }
+
+                    encryptedString += Alphabet[positionNumber];
+
+                    continue;
+                }
+
+                encryptedString += ch;
+            }
+
+            return encryptedString;
+        }
+
+        private static string DecryptString(string? sourceString, int slideNumber)
+        {
+            var decryptedString = string.Empty;
+
+            foreach (var ch in sourceString)
+            {
+                if (Alphabet.Contains(ch))
+                {
+                    var charPosition = GetCharPosition(ch);
+
+                    charPosition-= slideNumber % Alphabet.Length;
+
+                    if (charPosition < 0)
+                    {
+                        charPosition += Alphabet.Length;
+                    }
+
+                    decryptedString += Alphabet[charPosition];
+
+                    continue;
+                }
+
+                decryptedString += ch;
+            }
+
+            return decryptedString;
+        }
+
+        private static int GetCharPosition(char ch)
+        {
+            for (int i = 0; i < Alphabet.Length; i++)
+            {
+                if (ch == Alphabet[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ XorEncryption/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace XorEncryption;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var path = @"C:\Users\55873\OneDrive\Desktop\1.txt";
+
+        var bytes = File.ReadAllBytes(path);
+
+        //Key - random bytes
+        var maskList = new List<byte>();
+        maskList.Add(0b_0001_0100);
+        maskList.Add(0b_0001_0110);
+        maskList.Add(0b_0101_0100);
+
+        var j = 0;
+
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            if (j >= maskList.Count-1)
+            {
+                j = 0;
+            }
+
+            bytes[i] ^= maskList[j];
+
+            j++;
+        }
+
+        File.WriteAllBytes(path, bytes);
+
+        Console.Read();
+    }
+}
